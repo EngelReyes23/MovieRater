9ccf9fa1b7e5012c3a76873e272f166941769084
Engel Reyes <engelreyes23@gmail.com>
2021-12-11 22:37:02 -0600

    Cascaron basico

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import AppRoutes from "./components/routers/AppRoutes";
 
-function App() {
-  return (
-    <div className="App">
-      
-      <h1>Marcelo, cara de culo</h1>
-      
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-    </div>
-  );
-}
+const App = () => <AppRoutes />;
 
 export default App;
